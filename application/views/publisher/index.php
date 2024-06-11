1d368a2234777f850302b0afdc92124a5593429e
stevenoeb <rifqimuhsul@gmail.com>
2024-06-11 14:31:44 +0700

    commit

--- application/views/publisher/index.php
+++ application/views/publisher/index.php
@@ -5,7 +5,7 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
     <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
             <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>') ?>
             <?= $this->session->flashdata('message') ?>
             <table class="table table-hover">
