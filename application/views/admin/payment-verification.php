0f208fc07b24d7b6f40791b01293ba16b07033f2
ziyadoodle <ziyadzidane69@gmail.com>
2024-06-14 03:03:53 +0700

    add status and fix modal

--- application/views/admin/payment-verification.php
+++ application/views/admin/payment-verification.php
@@ -29,7 +29,7 @@
                         <td><?= $p['journal_path'] ?></td>
                         <td><a href="" class="badge badge-pill badge-primary" data-toggle="modal" data-target="#paymentModal<?= $i; ?>">Lihat Bukti Pembayaran</a></td>
                         <td>
-                            <p class="badge badge-success">Paid</p>
+                            <span class="badge <?= ($p['is_paid'] == "unpaid") ? "badge-danger" : "badge-success" ?>"><?= $p['is_paid']; ?></span>
                         </td>
                         <td>
                             <a href="#!" class="badge badge-success">Accept</a>
@@ -41,22 +41,16 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title w-100" id="paymentModalLabel">
-                                        Bukti Pembayaran
-                                    </h5>
+                                    <h5 class="modal-title w-100" id="paymentModalLabel">Bukti Pembayaran</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">
-                                            x
-                                        </span>
+                                        <span aria-hidden="true">x</span>
                                     </button>
                                 </div>
                                 <div class="modal-body">
                                     <img class="w-100" src="<?= base_url('assets/data/pembayaran/') . $p['image']; ?>" />
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                        Close
-                                    </button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                                 </div>
                             </div>
                         </div>
@@ -65,9 +59,6 @@
             </tbody>
         </table>
     </div>
-
-
-
 
 </div>
 <!-- /.container-fluid -->
