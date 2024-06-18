e5e6d388d2787267eef7637fd07d5eb3b26e2ada
ziyadoodle <ziyadzidane69@gmail.com>
2024-06-18 22:41:03 +0700

    fix table, add pagination for Publisher

--- application/models/Conference_model.php
+++ application/models/Conference_model.php
@@ -3,12 +3,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Conference_model extends CI_Model
 {
-    public function getSubmitPaper()
+    public function getSubmitPaper($limit, $start, $keyword = null)
     {
-        $query = "SELECT `conference_submissions`.*,`user`.`name`
-        FROM `conference_submissions` JOIN `user`
-        ON `conference_submissions`.`user_id`= `user`.`id`";
+        // $query = "SELECT `conference_submissions`.*,`user`.`name`
+        // FROM `conference_submissions` JOIN `user`
+        // ON `conference_submissions`.`user_id`= `user`.`id`";
 
-        return $this->db->query($query)->result_array();
+        // return $this->db->query($query)->result_array();
+        $this->db->select('conference_submissions.*, user.name');
+        $this->db->join('user', 'user_id = conference_submissions.user_id');
+        if ($keyword) {
+            $this->db->like('title', $keyword);
+            $this->db->or_like('name', $keyword);
+        }
+
+        return $this->db->get('conference_submissions', $limit, $start)->result_array();
     }
 }
