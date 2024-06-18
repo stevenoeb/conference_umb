f0adbb5e0be722deed0afe03eb901e1903c983ea
stevenoeb <rifqimuhsul@gmail.com>
2024-06-18 14:23:23 +0700

    country and university_user

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,7 +1,4 @@
-<!-- Begin Page Content -->
 <div class="container-fluid">
-
-    <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
     <div class="row">
@@ -13,20 +10,22 @@
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $user['image'] ?>" class="img-fluid rounded-start">
+                <img src="<?= base_url('assets/img/profile/') . $user['image'] ?>" class="img-fluid rounded-start" alt="Profile Picture">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title"><?= $user['name'] ?></h5>
-                    <p class="card-text"><?= $user['email'] ?></p>
-                    <p class="card-text"><small class="text-body-secondary">Member since <?= date('d F Y', $user['date_created']) ?></small></p>
+                    <p class="card-text"><strong>Email:</strong> <?= $user['email'] ?></p>
+                    <p class="card-text"><strong>Phone:</strong> <?= $user['phone'] ?></p>
+                    <p class="card-text"><strong>University:</strong> <?= $user['univ'] ?></p>
+                    <p class="card-text"><strong>Country:</strong> <?= $countries[$user['country']] ?></p>
+                    <p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']) ?></small></p>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
-<!-- /.container-fluid -->
+
 
 </div>
 <!-- End of Main Content -->
