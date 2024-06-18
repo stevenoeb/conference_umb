563a3d8ea0f634ad9f35a9af3c457d165d713ea0
ziyadoodle <ziyadzidane69@gmail.com>
2024-06-18 22:42:08 +0700

    fix query (typo)

--- application/models/Conference_model.php
+++ application/models/Conference_model.php
@@ -5,13 +5,8 @@ class Conference_model extends CI_Model
 {
     public function getSubmitPaper($limit, $start, $keyword = null)
     {
-        // $query = "SELECT `conference_submissions`.*,`user`.`name`
-        // FROM `conference_submissions` JOIN `user`
-        // ON `conference_submissions`.`user_id`= `user`.`id`";
-
-        // return $this->db->query($query)->result_array();
         $this->db->select('conference_submissions.*, user.name');
-        $this->db->join('user', 'user_id = conference_submissions.user_id');
+        $this->db->join('user', 'user.id = conference_submissions.user_id');
         if ($keyword) {
             $this->db->like('title', $keyword);
             $this->db->or_like('name', $keyword);
