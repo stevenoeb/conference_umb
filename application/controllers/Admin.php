6ee647ffb55cba202104d217c093411f8cdaacb7
ziyadoodle <ziyadzidane69@gmail.com>
2024-06-20 13:04:16 +0700

    fix bug

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -173,7 +173,6 @@ class Admin extends CI_Controller
         if ($this->input->post('accept')) {
             $id = $this->input->post('id');
             $this->db->set('is_accept', 'accepted');
-            $this->db->set('is_paid', 'pending');
             $this->db->where('id', $id);
             $this->db->update('conference_submissions');
             redirect('admin/verify_article/' . $data['start']);
