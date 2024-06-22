927f296c4b3556080106e9a595cff864acea0077
ziyadoodle <ziyadzidane69@gmail.com>
2024-06-22 09:15:43 +0700

    fix presenter views

--- application/views/presenter/index.php
+++ application/views/presenter/index.php
@@ -1,88 +1,89 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
+    <div class="flash-data" data-flashdata="<?= $this->session->flashdata('message') ?>"></div>
+    <?php if ($this->session->flashdata('message')) : ?>
 
+    <?php endif; ?>
     <section class="content">
-        <section class="content">
-            <div class="container-fluid">
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Detail Conference</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-5 col-lg-3 text-center">
-                                <img src="<?= base_url('assets/img/logo/LOGO.jpg') ?>" alt="User Pic" class="img-responsive rounded" style="width: 100%; height: auto">
-                            </div>
-                            <div class="col-md-9 col-lg-9">
-                                <br>
-                                <table class="table table-user-information table-sm">
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <strong>Event Name</strong>
-                                            </td>
-                                            <td>Accounting Conference on Sustainability and Technopreneurial International 2024</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Conference Name</strong>
-                                            </td>
-                                            <td>Accounting Challenges and Opportunities in The Global Era</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Category</strong>
-                                            </td>
-                                            <td>Accounting</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Published By</strong>
-                                            </td>
-                                            <td>-</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Indexed In</strong>
-                                            </td>
-                                            <td>-</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Reviewing Abstract</strong>
-                                            </td>
-                                            <td>International Conference and Community Service</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Reviewing Fullpaper</strong>
-                                            </td>
-                                            <td>International Conference and Community Service</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <strong>Presentatione</strong>
-                                            </td>
-                                            <td>International Conference and Community Service</td>
-                                        </tr>
-
-                                    </tbody>
-                                </table>
-                            </div>
+        <div class="container-fluid">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Detail Conference</h6>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-5 col-lg-3 text-center">
+                            <img src="<?= base_url('assets/img/logo/LOGO.jpg') ?>" alt="User Pic" class="img-responsive rounded" style="width: 100%; height: auto">
                         </div>
-                        <div class="row justify-content-center">
-                            <div class="col-md-5">
-                                <div class="box-footer">
-                                    <a href="<?= base_url('presenter/submitPaper') ?>" class="btn btn-lg btn-warning col-md-12" role="button">
-                                        <strong>SUBMIT YOUR PAPER</strong>
-                                    </a>
-                                </div>
+                        <div class="col-md-9 col-lg-9">
+                            <br>
+                            <table class="table table-user-information table-sm">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <strong>Event Name</strong>
+                                        </td>
+                                        <td>Accounting Conference on Sustainability and Technopreneurial International 2024</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Conference Name</strong>
+                                        </td>
+                                        <td>Accounting Challenges and Opportunities in The Global Era</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Category</strong>
+                                        </td>
+                                        <td>Accounting</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Published By</strong>
+                                        </td>
+                                        <td>-</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Indexed In</strong>
+                                        </td>
+                                        <td>-</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Reviewing Abstract</strong>
+                                        </td>
+                                        <td>International Conference and Community Service</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Reviewing Fullpaper</strong>
+                                        </td>
+                                        <td>International Conference and Community Service</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <strong>Presentatione</strong>
+                                        </td>
+                                        <td>International Conference and Community Service</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-5">
+                            <div class="box-footer">
+                                <a href="<?= base_url('presenter/submitPaper') ?>" class="btn btn-lg btn-warning col-md-12" role="button">
+                                    <strong>SUBMIT YOUR PAPER</strong>
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </section>
 
 
